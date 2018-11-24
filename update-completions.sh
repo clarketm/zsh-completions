b3f758def2798d1f74b8807c5961089f3cad5424
Travis Clarke <travis.m.clarke@gmail.com>
2018-11-23 22:04:25 -0800

    2018-11-23

--- update-completions.sh
+++ update-completions.sh
@@ -5,7 +5,12 @@ cwd=$( cd "$( dirname "$0" )" && pwd )
 git submodule update --recursive --remote
 
 cp -Lrf \
+"$cwd/nilsonholger_zsh-completions/_"* \
+"$cwd/src/"
+
+cp -Lrf \
 "$cwd/zchee_zsh-completions/src/zsh/_"* \
+"$cwd/zchee_zsh-completions/src/macOS/_"* \
 "$cwd/src/"
 
 cp -Lrf \
