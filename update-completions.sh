559c12c2bca65021ec05dfd0e6f01a39f88410ff
Travis Clarke <travis.m.clarke@gmail.com>
2019-07-17 20:27:21 -0700

    2019-07-17

--- update-completions.sh
+++ update-completions.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env sh
 
-cwd=$( cd "$( dirname "$0" )" && pwd )
+cwd=$(cd "$(dirname "$0")" && pwd)
 
 git submodule update --recursive --remote
 
 cp -Lrf \
-"$cwd/nilsonholger_zsh-completions/_"* \
-"$cwd/src/"
+    "$cwd/nilsonholger_zsh-completions/_"* \
+    "$cwd/src/"
 
 cp -Lrf \
-"$cwd/zchee_zsh-completions/src/zsh/_"* \
-"$cwd/zchee_zsh-completions/src/macOS/_"* \
-"$cwd/src/"
+    "$cwd/zchee_zsh-completions/src/zsh/_"* \
+    "$cwd/zchee_zsh-completions/src/macOS/_"* \
+    "$cwd/src/"
 
 cp -Lrf \
-"$cwd/zsh-users_zsh-completions/src/_"* \
-"$cwd/src/"
+    "$cwd/zsh-users_zsh-completions/src/_"* \
+    "$cwd/src/"
 
 cp -Lrf \
-"$cwd/override/_"* \
-"$cwd/src/"
+    "$cwd/override/_"* \
+    "$cwd/src/"
+
+git commit -am "$(date +%F)" && git push --tags origin/master
