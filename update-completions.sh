07e391289d0583aac177bd39bdd338f5046ba3db
Travis Clarke <travis.m.clarke@gmail.com>
2019-11-08 22:48:35 -0800

    2019-11-08

--- update-completions.sh
+++ update-completions.sh
@@ -23,5 +23,5 @@ cp -Lrf \
     "$cwd/src/"
 
 if [ "$1" != "--dry-run" ]; then
-	git pull && git add . && git commit -m "$(date +%F)" && git push --tags origin master || exit 0
+	git pull && git add . && git commit -m "$(date +%F)" && git push --tags origin master || echo "nothing to commit :)"
 fi
